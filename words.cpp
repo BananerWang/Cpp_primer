6be05957af6e63587129ec916ec5c39df8ecf137
BananerWang <276002869@qq.com>
2021-01-01 02:15:29 +0800

    implements find_urls

--- words.cpp
+++ words.cpp
@@ -8,6 +8,7 @@
 
 using std::vector; using std::string;
 using std::max; using std::transform;
+using std::search; using std::find;
 
 
 vector<string> split(const string& s)
@@ -58,12 +59,54 @@ bool equal(const std::string& s)
     return equal(s.begin(), s.end(), s.rbegin());
 }
 
+
+
+bool not_url_char(char c)
+{
+    static const string url_ch = "~;/?:@=&$-_.+!*'(),";
+
+    return !(isalnum(c) || find(url_ch.begin(), url_ch.end(), c) != url_ch.end());
+}
+
+string::const_iterator url_end(string::const_iterator b, string::const_iterator e)
+{
+    return find(b, e, not_url_char);
+}
+
+string::const_iterator url_beg(string::const_iterator b, string::const_iterator e)
+{
+    static const string sep = "://";
+    typedef string::const_iterator iter;
+    iter i = b;
+    while ((i = search(i, e, sep.begin(), sep.end())) != e) {
+        if (i != b  && i + sep.size() != e) {
+            iter beg = i;
+            while (beg != b && isalpha(beg[-1]))
+                beg--;
+            if (beg != i && not_url_char(i[sep.size()])) {
+                return beg;
+            }
+        }
+    }
+    return e;
+}
+
+
 vector<string> find_urls(const string& s)
 {
     vector<string> ret;
-
-
-
+    typedef string::const_iterator iter;
+    iter b = s.begin();
+    iter e = s.end();
+    while (b != e)
+    {
+        b = url_beg(b, e);
+        if (b != e) {
+            iter end = url_end(b, e);
+            ret.emplace_back(b, end);
+           b = end;
+        }
+    }
 
     return ret;
 
