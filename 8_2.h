241c03c21abd286e5051ff6faedae960ad5642c5
BananerWang <276002869@qq.com>
2021-01-16 16:31:14 +0800

    implements 8_2.h

--- 8_2.h
+++ 8_2.h
@@ -65,7 +65,63 @@ public:
 
     template<class In, class Out, class X> static Out remove_copy(In b, In e, Out d, const X& t)
     {
+        while (b != e)
+        {
+            if (*b != t)
+                *d++ = *b;
+            b++;
+        }
 
+    }
+
+    template<class In, class Out, class P> static Out remove_copy_if(In b, In e, Out d, P p)
+    {
+        while (b != e)
+        {
+            if (!p(*b))
+                *d++ = *b;
+            b++;
+        }
+        return d;
+    }
+    template<class Fw, class X> static Fw remove(Fw b, Fw e, const X& t)
+    {
+        Fw b_in = b;
+        while (b_in != e)
+        {
+            if (*b_in != t)
+                *b++ = *b_in;
+            b_in++;
+        }
+        return b;
+    }
+    template <class Fw, class P> static Fw transform(Fw, b, Fw e, P p)
+    {
+        Fw b_out = b;
+        while (b != e)
+        {
+            *b_out++ = p(*b++);
+        }
+        return b_out;
+    }
+    template <class Fw, class P> static Fw partition(Fw b, Fw e, P p)
+    {
+        Fw b_out = b;
+        while (b != e)
+        {
+            if (p(*b))
+                std::iter_swap(b, b_out++);
+            b++;
+        }
+        return b_out;
+    }
+    template <class In, class X> static X accumulate(In b, In e, X t)
+    {
+        while (b != e)
+        {
+            t += *b++;
+        }
+        return t;
     }
 
 };
